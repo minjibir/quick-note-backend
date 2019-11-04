9c9cb42a0d0c9fd5780288584c37ef1515b8ee62
Jabir S. Minjibir <jminjibir@swglobal.com>
2019-11-04 16:56:33 +0100

    Dockerfile added and CD enabled.

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const logger = require('morgan');
 const config = require('config');
 const express = require('express');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const noteRouter = require('./routes/note.route');
 
 const app = express();
-const PORT = process.env.NODE_ENV.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 
 // DB connection
 mongoose.connect(config.get('db.url'), { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Connected to ${config.get('db.scope')} datastore.`))
   .catch(() => console.log(`Unable to connect to ${config.get('db.scope')} datastore.`));
 
-// app.use(helmet);
+// Middlewares
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 
+// Application routes
 app.use('/users', usersRouter);
 app.use('/api/notes', noteRouter);
 
-module.exports = app.listen(PORT, () => console.log(`Server running at ${PORT}`));
+app.get('/', async (req, res) => {
+  res
+    .status(200)
+    .json({ result: 'Index page of Quick Note App' });
+});
+
+module.exPORTs = app.listen(PORT, () => console.log(`Server running on ${PORT}`));
